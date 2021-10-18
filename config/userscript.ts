9b9f7663cb8cc0690a4cb18a687dd0afe6557f7b
Simon Alling <alling.simon@gmail.com>
2021-10-18 10:31:00 +0200

    Version number 1.0.4

--- config/userscript.ts
+++ config/userscript.ts
@@ -1,7 +1,7 @@
 export default {
     id: "telia-wifi-auto-login",
     name: "Telia Wi-Fi Auto-Login",
-    version: "1.0.3",
+    version: "1.0.4",
     description: "Login to Telia Wi-Fi automatically.",
     author: "Simon Alling",
     hostname: "teliawifi.telia.com",
